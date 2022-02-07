dc45ae54dcb31d4dea517ae9c289b8306d0b09cc
Nathan Sutton <nasutton@gmail.com>
2022-02-07 16:54:50 -0500

    Update afterMigrate__0001_concept.sql

--- src/migrations/flyway/afterMigrate__0001_concept.sql
+++ src/migrations/flyway/afterMigrate__0001_concept.sql
@@ -1,5 +1,7 @@
 TRUNCATE flyway.concept;
 
-COPY flyway.concept FROM PROGRAM 'gunzip -c /opt/dim/CONCEPT.csv.gz' WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b' ;
+COPY flyway.concept 
+FROM PROGRAM 'gunzip -c /opt/dim/CONCEPT.csv.gz' 
+WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b' ;
 
 ANALYZE flyway.concept;
