f732bb50e65b665489d7c1ba8dee2c96e212e465
ns <nasutton@gmail.com>
2022-02-08 11:35:38 -0500

    format

--- src/migrations/sqitch/verify/concept.sql
+++ src/migrations/sqitch/verify/concept.sql
@@ -2,7 +2,12 @@
 
 BEGIN;
 
-SELECT 1/COUNT(*) FROM information_schema.tables WHERE table_name = 'concept' AND schema_name = 'sqitch'; -- exists
+SELECT 1/COUNT(*) FROM information_schema.tables 
+WHERE 
+    table_name = 'concept' 
+    AND schema_name = 'sqitch'
+; -- exists
+
 SELECT 1/COUNT(1) FROM sqitch.concept; -- has rows
 
 ROLLBACK;
